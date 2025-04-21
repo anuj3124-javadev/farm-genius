acdbe8ed9ff29cb6a532355d42097df57aa1627f
Anuj Yadav <anujyadavst24@gmail.com>
2025-04-21 10:23:28 +0530

    Update Weather.js
    some update

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -37,7 +37,7 @@ const Weather = () => {
       alert("Everything is going perfect");
     } catch (error) {
       alert("Error");
-      console.error("Error fetching weather data:", error);
+      
       setError("Could not fetch weather data. Try again.");
       setWeatherdata(null);
     }
